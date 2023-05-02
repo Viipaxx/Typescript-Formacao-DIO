a6681b5e2cd023210d704888f98ec0572c090162
PC <>
2023-05-02 18:23:39 -0300

    upando primeiro programa de ts

---
+++ app.ts
@@ -0,0 +1,8 @@
+
+
+
+const soma = (a: number, b: number) => {
+    console.log(a + b)
+}
+
+soma(1, 2)
---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+
+const soma = (a, b) => {
+    console.log(a + b)
+}
+
+soma(2, 2)
+soma('Natha', 'Souza')
