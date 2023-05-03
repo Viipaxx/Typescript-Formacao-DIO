f510383b3b9cd4f0e9e3eae95be919e95f80208b
Vitor Paz <kngatuno@gmail.com>
2023-05-03 00:49:07 -0300

    aplicando conceitos sobre módulos

--- app.ts
+++ app.ts
@@ -1,38 +1,8 @@
-
-abstract class Account {
-
-    name: string
-    accountNumber: number
-    balance: number = 0
-
-    constructor(name: string, accountNumber: number){
-        this.name = name
-        this.accountNumber = accountNumber
-    }
-
-    deposit = () => {
-        console.log('Você depositou!')
-    }
-
-    withdraw = () => {
-        console.log('Você sacou!')
-    }
-
-    getBalance = () => {
-        console.log(this.balance)
-    }
-
-}
-
-class PeopleAccount extends Account{
-    doc_id: number
-
-    constructor(doc_id:number, name:string, numberAccount: number){
-        super(name, numberAccount)
-        this.doc_id = doc_id
-    }
-}
-
+import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from './class/CompanyAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Vitor', 10)
-console.log(peopleAccount)
+peopleAccount.deposit()
+
+const compantAccount: CompanyAccount = new CompanyAccount('PicPay', 12)
+console.log(compantAccount.deposit())
